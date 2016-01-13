2cf010612a536e077060532c43e3711bbd90e4d4
Administrator <Administrator@AdministratordeMacBook-Pro.local>
2016-01-13 15:15:59 +0800

    LiHe

---
+++ LiHe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiHe
+//
+//  Created by Administrator on 16/1/13.
+//  Copyright © 2016年 XuYouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
